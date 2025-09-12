10197db4de78e4eb8fc3b269ca763cd321db82dc
Vikash Mishra <mishravikash0612@gmail.com>
2025-09-12 13:29:26 +0530

    sixth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import assets from '../assets/assets'
 import ThemeToggleBtn from './ThemeToggleBtn'
 
+
 const Navbar = ({ theme, setTheme }) => {
 
     const [sidebarOpen, setSidebarOpen] = useState(false)
